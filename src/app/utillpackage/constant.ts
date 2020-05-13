b9d419b9121637843fdfa5a3128b10dd2e04bb8b
Avinash Kumar <avinashkumar4656@gmail.com>
2020-05-13 18:05:08 +0530

    changes with build

--- src/app/utillpackage/constant.ts
+++ src/app/utillpackage/constant.ts
@@ -1,6 +1,8 @@
 export class MyConstants {
     // static BASEURL = "http://localhost:8081/api/";
-    static BASEURL = "http://3.21.83.36:8081/api/";
-    static IMAGESERVERURL ="http://3.21.83.36:8081/";
+    // static BASEURL = "http://3.21.83.36:8081/api/";
+    static BASEURL = "https://getithomenow.com:8081";
+    static IMAGESERVERURL ="https://getithomenow.com:8081";
+    // static IMAGESERVERURL ="http://3.21.83.36:8081/";
     
 }
