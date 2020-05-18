bc92d8d6273900e7d170d66c4ed1dd6c3c741e7e
Avinash Kumar <avinashkumar4656@gmail.com>
2020-05-18 15:02:22 +0530

    url changes with build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -66,7 +66,7 @@ import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
   ],
   providers: [
     { provide: AosToken, useValue: aos },
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    // { provide: LocationStrategy, useClass: HashLocationStrategy },
     CookieService,
     GoogleMapsAPIWrapper 
   ],
