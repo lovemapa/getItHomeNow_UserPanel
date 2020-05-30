23a7155fa81709dd14918a73f573abcef86fdc9a
Avinash kumar <avinashkumar4656@gmail.com>
2020-05-30 18:57:54 +0530

    delivery changes

--- src/app/componentPackage/delivery/delivery.component.ts
+++ src/app/componentPackage/delivery/delivery.component.ts
@@ -8,7 +8,9 @@ import { CookieService } from 'ngx-cookie-service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import itemListData from '../../../assets/arraylocation/itemList.json';
 import timedata from '../../../assets/arraylocation/timeList.json';
-
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 declare var google: any;
 @Component({
   selector: 'app-delivery',
@@ -55,13 +57,26 @@ export class DeliveryComponent implements OnInit {
   priceForDelivery: string = '$99/hr';
   timearrayList: Array<any>
   selectedTimeSlot: string;
+  formGroup: FormGroup;
+  titleAlert: string = 'This field is required';
+  post: any = '';
+  username: any
+  hideRequiredControl = new FormControl(false);
+  floatLabelControl = new FormControl('auto');
+  selecteddatebutton = false;
+  rightNowButton = false;
+  buttonNameDataSection:any
+
+
   constructor(private mapsAPILoader: MapsAPILoader, public ngZone: NgZone, public userBackEndService: UserpanelServiceService,
-    public modalService: NgbModal, public cookiesSerive: CookieService, public spinner: NgxSpinnerService) {
+    public modalService: NgbModal, public cookiesSerive: CookieService, public spinner: NgxSpinnerService,
+    private formBuilder: FormBuilder) {
     this.origin = "";
     this.destination = "";
     this.phonenumber = "";
     this.email = "";
     this.name = "";
+    this.username = "";
     this.pickUpLocationName = "";
     this.destinationLocationName = "";
     this.searchingValue = "";
@@ -72,6 +87,8 @@ export class DeliveryComponent implements OnInit {
     this.showItemPage = false;
     this.dateSelectedPage = false;
     this.showtimefalse = false;
+    this.selecteddatebutton = true;
+    this.rightNowButton = false;
     this.selectedLocationIds = [];
     this.itemList = [];
     this.selectedItemList = [];
@@ -83,12 +100,18 @@ export class DeliveryComponent implements OnInit {
     this.iconDisplay = "fa fa-arrow-circle-up";
     this.previousId = "";
     this.timearrayList = [];
+    this.buttonNameDataSection="selectbydatetime"
 
 
     if (this.itemList.length == 0) {
       this.loadMap()
       this.getSelectedLocationIds();
     }
+
+    this.formGroup = formBuilder.group({
+      hideRequired: this.hideRequiredControl,
+      floatLabel: this.floatLabelControl,
+    });
 
     // this.userBackEndService.getItemLists().subscribe(responseData => {
     //   this.itemList = responseData;
@@ -100,11 +123,14 @@ export class DeliveryComponent implements OnInit {
     // this.dateSelectedPage=true;
     // this.formFieldshow=false;
     // this.getDateNextThree();
+    // this.itemList = itemListData;
+    // this.getDateNextThree();
+    // this.dateSelectedPage = true
 
   }
 
   ngOnInit(): void {
-
+    this.createForm();
   }
 
   /**
@@ -328,6 +354,21 @@ export class DeliveryComponent implements OnInit {
       this.showItemPage = false;
       this.timearrayList = timedata;
       this.getDateNextThree();
+      
+
+        if(this.selecteddatebutton == true)
+        {
+             if(this.selectedDate != null)
+             {
+                 this.selectedDate=this.selectedDate;
+             }else{
+                 this.selectedDate=null;
+             }
+             this.rightNowButton=false
+        }else{
+          this.rightNowButton=true
+        }
+
       this.dateSelectedPage = true;
 
     } else if (this.dateSelectedPage) {
@@ -335,6 +376,8 @@ export class DeliveryComponent implements OnInit {
       this.formFieldshow = true;
 
     } else if (this.formFieldshow == true) {
+
+      alert(JSON.stringify(this.formGroup.value))
       this.formFieldshow = false;
       this.showFinallist = true;
 
@@ -347,26 +390,7 @@ export class DeliveryComponent implements OnInit {
    * Method to Reset all the Values 
    */
   clickReset() {
-    this.pickupLocation = true;
-    this.destinationboolean = false;
-    this.dateSelectedPage = false;
-    this.selectedDate = null;
-    this.name = "";
-    this.email = "";
-    this.phonenumber = "";
-    this.formFieldshow = false;
-    this.showFinallist = false
-    this.dateList = [];
-    this.origin = "";
-    this.destination = "";
-    this.pickUpLocationName = "";
-    this.itemList = [];
-    this.temparyNameDesination = "";
-    this.destinationLocationName = "";
-    this.searchElementRef.nativeElement.value = "";
-    this.textboxLabel = "Pick Up Address";
-    this.textBoxPLaceBolder = "Enter PickUp Address"
-    this.iconDisplay = "fa fa-arrow-circle-up";
+    this.opensweetalert()
   }
 
   /**
@@ -385,7 +409,11 @@ export class DeliveryComponent implements OnInit {
     if (this.dateSelectedPage && this.selectedDate != null) {
       return false;
     }
-    if (this.formFieldshow && this.name.trim().length != 0 && this.email.trim().length != 0 && this.phonenumber.trim().length != 0) {
+    // if (this.formFieldshow && this.name.trim().length != 0 && this.email.trim().length != 0 && this.phonenumber.trim().length != 0) {
+    //   CommonMethods.showconsole(this.Tag, "Function")
+    //   return false;
+    // }
+    if (this.formFieldshow && this.formGroup.valid) {
       CommonMethods.showconsole(this.Tag, "Function")
       return false;
     }
@@ -419,7 +447,7 @@ export class DeliveryComponent implements OnInit {
   getDateNextThree() {
     this.dateList = [];
     let today = new Date();
-    if(today.getHours()>20){
+    if (today.getHours() > 20) {
       today.setDate(today.getDate() + 1);
     }
     this.dateList.push({
@@ -470,7 +498,7 @@ export class DeliveryComponent implements OnInit {
       this.selectedItemList.splice(index, 1);
     }
 
-    this.cookiesSerive.set("selected_productList", JSON.stringify(this.selectedItemList))
+    // this.cookiesSerive.set("selected_productList", JSON.stringify(this.selectedItemList))
 
   }
 
@@ -488,7 +516,7 @@ export class DeliveryComponent implements OnInit {
         if (this.selectedDate.getDate() == new Date().getDate()) {
           let timeSlotStartHour: number = 0;
           if (this.selectedDate.getMinutes() > 30) {
-            timeSlotStartHour = this.selectedDate.getHours()+1;
+            timeSlotStartHour = this.selectedDate.getHours() + 1;
           }
           else {
             timeSlotStartHour = this.selectedDate.getHours();
@@ -520,13 +548,123 @@ export class DeliveryComponent implements OnInit {
 
   }
 
-  keyPress(event: any) {
-    const patt = /^[0-9]{1,4}(\.[0-9][0-9])?$/
+
+
+  /** Form Input Validated */
+
+
+
+  createForm() {
+    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    this.formGroup = this.formBuilder.group({
+      'email': [null, [Validators.required, Validators.pattern(emailregex)]],
+      'name': [null, Validators.required],
+      'mobileNumber': [null, [Validators.required , Validators.min(9), Validators.max(10)]],
+      'any_special_instruction': [null]
+      // 'mobileNumber': [null, [Validators.required, Validators.pattern('[6-9]\\d{9}')]]
+    });
+  }
+
+
+
+
+
+
+
+
+
+  getErrorEmail() {
+    return this.formGroup.get('email').hasError('required') ? 'Email address is required' :
+      this.formGroup.get('email').hasError('pattern') ? 'Email address is not valid' : '';
+
+  }
+
+  _keyPress(event: any) {
+    const pattern = /[0-9]/;
     let inputChar = String.fromCharCode(event.charCode);
-    if (event.keyCode != 8 && !patt.test(inputChar)) {
+    if (!pattern.test(inputChar)) {
       event.preventDefault();
+
     }
   }
+
+
+
+  /**Click Method Selected date and right now button*/
+  selctedDateCategory(buttonname:any){
+    this.buttonNameDataSection=buttonname;
+    if(buttonname == 'selectbydatetime' && this.selecteddatebutton == false)
+    { 
+
+      let newemptyvale:Date
+      this.selectedDate=newemptyvale
+      this.getDateNextThree()
+      this.selecteddatebutton=true;
+      this.rightNowButton=false
+    }else{
+      this.timearrayList=[]
+      this.selectedTimeSlot=""
+      this.selectedDate= new Date()
+      this.selecteddatebutton=false;
+      this.rightNowButton=true
+    }
+
+  }
+
+
+
+    /**
+   * Sweet Alert   Show    
+   */
+  opensweetalert() {
+    Swal.fire({
+      title: "Are you sure?",
+      text: " you want to reset All changes will be lost!",
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        this.dataReset();
+      } else {
+           CommonMethods.showconsole(this.Tag,"Else is working")
+      }
+    });
+  }
+
+
+
+  /**Data Reset */
+dataReset()
+{
+  this.pickupLocation = true;
+  this.destinationboolean = false;
+  this.dateSelectedPage = false;
+  this.selectedDate = null;
+  // this.name = "";
+  // this.email = "";
+  // this.phonenumber = "";
+  this.searchingValue=""
+  this.formFieldshow = false;
+  this.showFinallist = false
+  this.dateList = [];
+  this.origin = "";
+  this.destination = "";
+  this.selectedTimeSlot=""
+  this.pickUpLocationName = "";
+  this.itemList = [];
+  this.selectedItemList = [];
+  this.searchItemList=[];
+  this.temparyNameDesination = "";
+  this.destinationLocationName = "";
+  this.searchElementRef.nativeElement.value = "";
+  this.textboxLabel = "Pick Up Address";
+  this.textBoxPLaceBolder = "Enter PickUp Address"
+  this.iconDisplay = "fa fa-arrow-circle-up";
+  this.setCurrentLocation()
+}
+
+
 
 
 }
